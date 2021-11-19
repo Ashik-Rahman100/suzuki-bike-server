3c5f12d4a760d47ace9924841a3d3120f2d0d8b9
Ashik-Rahman100e <ashikrahman5515u@gmail.com>
2021-11-19 22:25:00 +0600

    review, booking and admin update

--- index.js
+++ index.js
@@ -27,23 +27,83 @@ async function run(){
 
         await client.connect();
         const database = client.db("bikeStore");
-        const serviceCollection = database.collection("products");
+        const productCollection = database.collection("products");
+        const reviewCollection = database.collection("reviews");
+        const bookingCollection = database.collection("booking");
+        const adminCollection = database.collection('admin');
+
+
         
-        // get data
+        
+        // get Product data
         app.get('/products', async(req, res) =>{
-            const cursor = serviceCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
 
-
-        // Post data
+        // Post Product data
         app.post('/products', async(req, res) =>{
             console.log('hitting the post' , req.body);
             const newPlace = req.body;
-            const result = await serviceCollection.insertOne(newPlace);
+            const result = await productCollection.insertOne(newPlace);
             console.log(result);
             res.json(result);
         })
+
+        // get Reviews data
+        app.get('/reviews', async(req, res) =>{
+            const cursor = reviewCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        // Post Product data
+        app.post('/reviews', async(req, res) =>{
+            console.log('hitting the post' , req.body);
+            const newReview = req.body;
+            const result = await reviewCollection.insertOne(newReview);
+            console.log(result);
+            res.json(result);
+        });
+
+        // Add Booking
+        app.post("/addbook", async(req, res) =>{
+            const booking = req.body;
+            console.log("booking",booking);
+            bookingCollection.insertOne(booking).then((result) =>{
+                res.send(result)
+            })
+        });
+
+        // Post Booking
+        app.get("/addBook", async(req, res) =>{
+            const email = req.query.email;
+            const query = {email:email}
+            const cursor = bookingCollection.find(query);
+            const booking = await cursor.toArray();
+            res.send(booking)
+        });
+
+         //delete booking product
+         app.delete("/addBook/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            console.log(id);
+            const result = await bookingCollection.deleteOne(query);
+            console.log("Deliting id",result);
+            res.json(result)
+         
+        });
+
+        // make Admin
+        app.put('/admin', async(req,res)=>{
+            const user = req.body;
+            console.log('user', user);
+            const filter = {email:user.email};
+            const updateDoc = {$set: {role:'admin'}};
+            const result = await adminCollection.updateOne(filter,updateDoc);
+            res.send(result);
         });
 
     }
